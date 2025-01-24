7db1e07d42fae1329709d3aee364340f626bf877
Satwik Chalasani <satwikchalasani@Satwiks-MacBook-Pro.local>
2025-01-24 14:17:05 -0600

    Added basic backend with api and dummy cars data

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const cars = require('./cars.json');
+
+app.get('/cars', (req, res) => {
+  return res.json(cars)
+});
+
+app.get('/car/:id', (req, res) => {
+    const carId = parseInt(req.params.id);
+    const car = cars.find(c => c.id === carId);
+    
+    if (car) {
+      return res.json(car);
+    } else {
+      res.status(404).json({ error: 'Car not found' });
+    }
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
